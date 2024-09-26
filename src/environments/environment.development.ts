bd2cbeaf7b85634b1275f96e76a64d9bfc384566
muhammedabdelhamed <128986303+muhammedabdelhamed@users.noreply.github.com>
2024-09-26 17:23:33 +0300

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production:false,
   // BAseApiURL:"https://walmart-api-zl4b.onrender.com"
-  // BAseApiURL:"https://walmart-api-zl4b.onrender.com"
-  BAseApiURL:"http://localhost:3000"
+  BAseApiURL:"https://walmart-api-zl4b.onrender.com"
+  // BAseApiURL:"http://localhost:3000"
 };
